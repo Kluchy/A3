25fe1af3a338b06851d693d58304867c00c1c38e
Kluchy <kfm53@cornell.edu>
2016-03-04 20:18:12 -0500

    indentation of Bob

--- src/Bob.java
+++ src/Bob.java
@@ -10,46 +10,47 @@ public class Bob extends Principal {
 	private static final String BPRIFILE = "priKB.txt";
 	private static final String APUBFILE = "pubKA.txt";
 
-	
+
 	public Bob(String portNumber) throws UnknownHostException, IOException {
 		pubK = readPubKey(BPUBFILE);
 		privK = readPriKey(BPRIFILE);
 		otherPubK1 = readPubKey(APUBFILE);
 		otherPubK2 = null;
 		S = "Bob>> ";
-//		if (pubK==null || privK==null || pubKB==null)
 	}
-	
+
 	public static void main(String args[]) {
 		//bob's port number is just 8080
 		int port = 8080;
-        ServerSocket serverBob = null;
-        String line;
-        BufferedReader inputStream;
-        Socket clientSocket = null;
+		ServerSocket serverBob = null;
+		String line;
+		BufferedReader inputStream;
+		Socket clientSocket = null;
 		// Try to open a server socket on port
-        try {
-           serverBob = new ServerSocket(port);
-        }
-        catch (IOException e) {
-           System.out.println(e);
-        }   
+		try {
+			serverBob = new ServerSocket(port);
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}   
 		// Create a socket object. Accept connections. Open in stream
-	    try {
-	           clientSocket = serverBob.accept();
-	           inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	           //bob just prints whatever he receives...
-	           System.out.println("Bob >>");
-	           while (true) {
-	             line = inputStream.readLine();
-	             System.out.println(line); 
-	           }
-	        }   
-		    catch (IOException e) {
-		           System.out.println(e);
-		        }
-		    }
-	
+		try {
+			clientSocket = serverBob.accept();
+			inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			//bob just prints whatever he receives...
+			System.out.println("Bob >>");
+			while (true) {
+				if (inputStream.ready()) {
+					line = inputStream.readLine();
+					System.out.println(line); 
+				}
+			}
+		}   
+		catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
 	//old main function
 	/*
 	public static void main(String[] args) {
@@ -72,5 +73,5 @@ public class Bob extends Principal {
 		} finally {
 		}
 	}
-	*/
+	 */
 }
